061ad6a733648995a6fb6136b870e075ac44e347
Lee Dohm <lee@liftedstudios.com>
2013-07-30 09:23:23 -0700

    Just after 'rails new'

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Bl2SkillCalculator::Application.initialize!
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bl2SkillCalculator::Application.config.session_store :cookie_store, key: '_bl2-skill-calculator_session'
